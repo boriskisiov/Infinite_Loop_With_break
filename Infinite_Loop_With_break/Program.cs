5774774cb47950ca77b1293d704f2829ab11fda9
boriskisiov <boriskisiov@gmail.com>
2023-10-23 16:29:55 +0300

    Add project files.

---
+++ Infinite_Loop_With_break/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Infinite_Loop_With_break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End") {
+                    break;
+                }
+                Console.WriteLine("Executing: " + command);
+            }
+            
+        }
+    }
+}
